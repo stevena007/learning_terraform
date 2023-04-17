a4627f751b77c1162205a246db730f2c2695e798
Steve Agent <steve.agent@sparq.com.au>
2023-04-18 09:32:01 +1000

    attach security group to the EC2 instance

--- main.tf
+++ main.tf
@@ -24,6 +24,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "Learning Terraform"
   }
@@ -38,7 +40,7 @@ resource "aws_security_group" "blog" {
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
-  in_port     = 80
+  to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -48,7 +50,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
-  in_port     = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -58,7 +60,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 resource "aws_security_group_rule" "everything_out" {
   type        = "egress"
   from_port   = 0
-  in_port     = 0             # all ports
+  to_port     = 0             # all ports
   protocol    = "-1"          # all protocols
   cidr_blocks = ["0.0.0.0/0"] # all ip ranges
 
