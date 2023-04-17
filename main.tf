d02109626653f8fa45a7463c7eccc7401d473160
Steve Agent <steve.agent@sparq.com.au>
2023-04-18 09:21:12 +1000

    Added Security Groups to the default VPC

--- main.tf
+++ main.tf
@@ -14,11 +14,56 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
+
+resource "aws_security_group" "blog" {
+  name        = "blog-sg"
+  description = "Allow HTTP and HTTPs in, allow everything out"
+  vpc_id      = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  in_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  in_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "everything_out" {
+  type        = "egress"
+  from_port   = 0
+  in_port     = 0             # all ports
+  protocol    = "-1"          # all protocols
+  cidr_blocks = ["0.0.0.0/0"] # all ip ranges
+
+  security_group_id = aws_security_group.blog.id
+}
+
+
+
