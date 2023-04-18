1a7b1f9dae9274e8c8ddcafa2af9da8daca2fbe9
Steve Agent <steve.agent@sparq.com.au>
2023-04-18 10:15:50 +1000

    Use AWS terraform module to create dev VPC

--- main.tf
+++ main.tf
@@ -14,12 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -31,12 +25,31 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+
+}
+
 module "blog_new_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
